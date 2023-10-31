987f9e092e63e740989826e1998685f01b717e2f
dashboy <dashiellboyles@hotmail.com>
2023-10-31 14:00:50 +0000

    added colors to each lines

--- components/LineStatus.tsx
+++ components/LineStatus.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function LineStatus({ line }: any) {
+  const getBorderColor = () => {
+    switch (line.name) {
+      case 'Bakerloo':
+        return 'darkbrown';
+      case 'Central':
+        return 'red';
+      case 'Circle':
+        return 'yellow';
+      case 'District':
+        return 'green';
+      case 'DLR':
+        return 'turquoise';
+      case 'Hammersmith & City':
+        return 'pink';
+      case 'Jubilee':
+        return 'gray';
+      case 'Metropolitan':
+        return 'magenta';
+      case 'Northern':
+        return 'black';
+      case 'Piccadilly':
+        return 'darkblue';
+      case 'Victoria':
+        return 'blue';
+      case 'Waterloo & City':
+        return 'turquoise';
+    }
+  };
+
+  const lineBorderColor = getBorderColor();
+
+  const styles = StyleSheet.create({
+    lineContainer: {
+      flex: 1,
+      borderColor: lineBorderColor,
+      padding: 10,
+      marginBottom: 10,
+      borderWidth: 5,
+      borderRadius: 20
+    }
+  })
+
   return (
-    <View>
+    <View style={styles.lineContainer}>
       <Text>{line.name}</Text>
       {line.disruptions.length > 0 ? (
         line.disruptions.map((disruption: any) => (
